4f74143ffa0624ec02d595c1b9f2177ac4b0e661
Jeny <jeneeshakasundi94@gmail.com>
2019-04-04 14:43:20 +0530

    rabbit mq

--- src/main/scala/bookStore/BookClient.scala
+++ src/main/scala/bookStore/BookClient.scala
@@ -1,28 +1,42 @@
 package bookStore
-import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.{ConnectionFactory, DeliverCallback}
 
 object BookClient {
 
-  private val EXCHANGE_NAME = "logs"
-  var function : String = _
-  var message = new StringBuilder()
+  private val EXCHANGE_NAME = "directExchange"
+  private val BINDING_KEY = "request"
+  private val BINDING_KEY_RESPONSE = "response"
 
   def main(argv: Array[String]) {
-    System.out.println("Enter the function \n\tView All \n\tfindById \n\tremoveById \n\tsave \n\tupdate \n: ")
+    System.out.println("inside main ----------------------------------------------------------------------------")
+    getUserInput()
+  }
+
+  def getUserInput(): Unit ={
+    System.out.println("inside getUserInput ########################################################################")
+
+    var message = new StringBuilder()
+    //var function : String = ""
+    System.out.print("Enter the function \n\tView All \n\tfindById \n\tremoveById \n\tsave \n\tupdate \n: ")
     val input = scala.io.StdIn.readLine()
-    function = input.toLowerCase()
+    var function = input.toLowerCase()
     message = message.append(function)
 
     if (function.equalsIgnoreCase("view all")){
-
+      createConnection(message.toString())
+      getResponse()
     }else if (function.equalsIgnoreCase("findById")){
-      System.out.println("Enter Id : ")
+      System.out.print("Enter Id : ")
       val id = scala.io.StdIn.readLine()
       message = message.append(",").append(id)
+      createConnection(message.toString())
+      getResponse()
     }else if (function.equalsIgnoreCase("removeById")){
-      System.out.println("Enter Id : ")
+      System.out.print("Enter Id : ")
       val id = scala.io.StdIn.readLine()
       message = message.append(",").append(id)
+      createConnection(message.toString())
+      getResponse()
     }else if (function.equalsIgnoreCase("save")){
       System.out.print("Enter Book Name : ")
       val bookName = scala.io.StdIn.readLine()
@@ -33,6 +47,8 @@ object BookClient {
       System.out.print("Enter Book Genre : ")
       val bookGenre = scala.io.StdIn.readLine()
       message = message.append(",").append(bookName).append(",").append(authorName).append(",").append(price).append(",").append(bookGenre)
+      createConnection(message.toString())
+      getResponse()
     }else if (function.equalsIgnoreCase("update")){
       System.out.print("Enter Book Id : ")
       val bookId = scala.io.StdIn.readLine()
@@ -45,9 +61,10 @@ object BookClient {
       System.out.print("Enter Book Genre : ")
       val bookGenre = scala.io.StdIn.readLine()
       message = message.append(",").append(bookId).append(",").append(bookName).append(",").append(authorName).append(",").append(price).append(",").append(bookGenre)
+      createConnection(message.toString())
+      getResponse()
     }
-    createConnection(message.toString())
-
+    //createConnection(message.toString())
 
   }
 
@@ -56,12 +73,31 @@ object BookClient {
     factory.setHost("localhost")
     val connection = factory.newConnection()
     val channel = connection.createChannel()
-    channel.exchangeDeclare(EXCHANGE_NAME, "fanout")
-    channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"))
+    channel.exchangeDeclare(EXCHANGE_NAME, "direct")
+    channel.basicPublish(EXCHANGE_NAME, BINDING_KEY, null, message.getBytes("UTF-8"))
     println(" [x] Sent '" + message + "'")
     channel.close()
     connection.close()
   }
 
+  def getResponse() : Unit = {
+    System.out.println("inside getResponse*******************************************************************")
 
+    val factory = new ConnectionFactory()
+    factory.setHost("localhost")
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+    channel.exchangeDeclare(EXCHANGE_NAME, "direct")
+    val queueName = channel.queueDeclare().getQueue
+    channel.queueBind(queueName, EXCHANGE_NAME, BINDING_KEY_RESPONSE)
+
+    val deliverCallback: DeliverCallback = (_, delivery) => {
+      val message = new String(delivery.getBody, "UTF-8")
+      System.out.println(message + "\n")
+      getUserInput()
+
+    }
+    channel.basicConsume(queueName, true, deliverCallback, _ => {})
+    //getUserInput()
+  }
 }
--- src/main/scala/bookStore/BookServer.scala
+++ src/main/scala/bookStore/BookServer.scala
@@ -1,7 +1,10 @@
 package bookStore
 import com.rabbitmq.client._
 object BookServer {
-  private val EXCHANGE_NAME = "logs"
+  private val EXCHANGE_NAME = "directExchange"
+  private val BINDING_KEY = "request"
+  private val BINDING_KEY_RESPONSE = "response"
+  var responseMessage : String = _
 
   def main(argv: Array[String]) {
 
@@ -9,38 +12,53 @@ object BookServer {
     factory.setHost("localhost")
     val connection = factory.newConnection()
     val channel = connection.createChannel()
-    channel.exchangeDeclare(EXCHANGE_NAME, "fanout")
+    channel.exchangeDeclare(EXCHANGE_NAME, "direct")
     val queueName = channel.queueDeclare().getQueue
-    channel.queueBind(queueName, EXCHANGE_NAME, "")
-    println(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.queueBind(queueName, EXCHANGE_NAME, BINDING_KEY)
 
     val deliverCallback: DeliverCallback = (_, delivery) => {
       val message = new String(delivery.getBody, "UTF-8")
       val array = message.split(",")
       val function = array(0)
       if (function.equalsIgnoreCase("View All")){
-        System.out.println(BookStoreApp.retrieveAll())
+        responseMessage = BookStoreApp.retrieveAll().toString
+        createConnection(responseMessage)
       }else if (function.equalsIgnoreCase("findById")){
-        System.out.println(BookStoreApp.findById(array(1).toInt))
+        responseMessage = BookStoreApp.findById(array(1).toInt).toString
+        createConnection(responseMessage)
       }else if (function.equalsIgnoreCase("removeById")){
         BookStoreApp.removeById(array(1).toInt)
-        System.out.println("Removed")
+        responseMessage = "REMOVED"
+        createConnection(responseMessage)
       }else if (function.equalsIgnoreCase("save")){
-        val b = Book(array(1),array(2),array(3).toDouble,array(4))
-        BookStoreApp.save(b)
-        System.out.println("Saved " + b)
+        val book = Book(array(1),array(2),array(3).toDouble,array(4))
+        BookStoreApp.save(book)
+        responseMessage = "Saved " + book + "successfully"
+        createConnection(responseMessage)
       }else if (function.equalsIgnoreCase("update")){
         val id = array(1).toInt
-        val b = Book(array(2),array(3),array(4).toDouble,array(5))
-        BookStoreApp.updateBookData(id,b)
-        System.out.println("Updated " + b)
+        val book = Book(array(2),array(3),array(4).toDouble,array(5))
+        BookStoreApp.updateBookData(id,book)
+        responseMessage = "Updated " + book
+        createConnection(responseMessage)
       }
+      //createConnection(responseMessage)
     }
     channel.basicConsume(queueName, true, deliverCallback, _ => {})
   }
 
 
-
+  def createConnection (message : String) : Unit = {
+    val factory = new ConnectionFactory()
+    factory.setHost("localhost")
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+    channel.exchangeDeclare(EXCHANGE_NAME, "direct")
+    channel.basicPublish(EXCHANGE_NAME, BINDING_KEY_RESPONSE, null, message.getBytes("UTF-8"))
+    println(" [x] Sent '" + message + "'")
+    channel.close()
+    connection.close()
+  }
 
 
 
