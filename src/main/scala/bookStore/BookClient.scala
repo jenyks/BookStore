374e764b93a1e3c8745f9a3f90c90961c38c28d5
Jeny <jeneeshakasundi94@gmail.com>
2019-04-05 15:23:55 +0530

    rabbit mq

--- src/main/scala/bookStore/BookClient.scala
+++ src/main/scala/bookStore/BookClient.scala
@@ -5,7 +5,6 @@ object BookClient {
 
   private val EXCHANGE_NAME = "directExchange"
   private val EXCHANGE_NAME_RES  = "directExchange2"
-
   private val BINDING_KEY = "request"
   private val BINDING_KEY_RESPONSE = "response"
   val factory = new ConnectionFactory()
@@ -18,9 +17,8 @@ object BookClient {
   }
 
   def getUserInput(): Unit ={
-
     var message = new StringBuilder()
-    System.out.print("Enter the function \n\tView All \n\tfindById \n\tremoveById \n\tsave \n\tupdate \n: ")
+    System.out.print("Enter the function \n\tView All \n\tsave \n\tfindById \n\tupdate \n\tremoveById \n: ")
     val input = scala.io.StdIn.readLine()
     var function = input.toLowerCase()
     message = message.append(function)
@@ -62,38 +60,27 @@ object BookClient {
       val bookGenre = scala.io.StdIn.readLine()
       message = message.append(",").append(bookId).append(",").append(bookName).append(",").append(authorName).append(",").append(price).append(",").append(bookGenre)
     }
-
     createConnection(message.toString())
-
   }
 
   def createConnection (message : String) : Unit = {
-
     val channel = connection.createChannel()
     channel.exchangeDeclare(EXCHANGE_NAME, "direct")
     channel.basicPublish(EXCHANGE_NAME, BINDING_KEY, null, message.getBytes("UTF-8"))
-    println(" [x] Sent '" + message + "'")
-
-    //getResponse()
   }
 
   def getResponse() : Unit = {
-
     val channel2 = connection.createChannel()
     channel2.exchangeDeclare(EXCHANGE_NAME_RES, "direct")
     val queueName = channel2.queueDeclare().getQueue
-
     channel2.queueBind(queueName, EXCHANGE_NAME_RES, BINDING_KEY_RESPONSE)
 
     val deliverCallback: DeliverCallback = (_, delivery) => {
       val message = new String(delivery.getBody, "UTF-8")
       System.out.println(message + "\n")
-
       getUserInput()
     }
-
     channel2.basicConsume(queueName, false, deliverCallback, _ => {})
   }
-
 
 }
--- src/main/scala/bookStore/BookServer.scala
+++ src/main/scala/bookStore/BookServer.scala
@@ -3,7 +3,6 @@ import com.rabbitmq.client._
 object BookServer {
   private val EXCHANGE_NAME = "directExchange"
   private val EXCHANGE_NAME_RES  = "directExchange2"
-
   private val BINDING_KEY = "request"
   private val BINDING_KEY_RESPONSE = "response"
   var responseMessage : String = _
@@ -11,9 +10,7 @@ object BookServer {
   factory.setHost("localhost")
   val connection = factory.newConnection()
 
-
   def main(argv: Array[String]) {
-
     val channel = connection.createChannel()
     channel.exchangeDeclare(EXCHANGE_NAME, "direct")
     val queueName = channel.queueDeclare().getQueue
@@ -52,16 +49,11 @@ object BookServer {
     channel.basicConsume(queueName, true, deliverCallback, _ => {})
   }
 
-
   def createReplyConnection (message : String) : Unit = {
     val channel2 = connection.createChannel()
     channel2.exchangeDeclare(EXCHANGE_NAME_RES, "direct")
     channel2.basicPublish(EXCHANGE_NAME_RES, BINDING_KEY_RESPONSE, null, message.getBytes("UTF-8"))
     println(" [x] Sent '" + message + "'")
   }
-
-
-
-
 
 }
