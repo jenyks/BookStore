1d79d4107d3f65233fb1866c25ae8fec9392968d
Jeny <jeneeshakasundi94@gmail.com>
2019-04-05 13:31:57 +0530

    rabbit mq

--- src/main/scala/bookStore/BookServer.scala
+++ src/main/scala/bookStore/BookServer.scala
@@ -2,14 +2,17 @@ package bookStore
 import com.rabbitmq.client._
 object BookServer {
   private val EXCHANGE_NAME = "directExchange"
+  private val EXCHANGE_NAME_RES  = "directExchange2"
+
   private val BINDING_KEY = "request"
   private val BINDING_KEY_RESPONSE = "response"
   var responseMessage : String = _
+  val factory = new ConnectionFactory()
+  factory.setHost("localhost")
+  //val connection = factory.newConnection()
+
 
   def main(argv: Array[String]) {
-
-    val factory = new ConnectionFactory()
-    factory.setHost("localhost")
     val connection = factory.newConnection()
     val channel = connection.createChannel()
     channel.exchangeDeclare(EXCHANGE_NAME, "direct")
@@ -22,42 +25,40 @@ object BookServer {
       val function = array(0)
       if (function.equalsIgnoreCase("View All")){
         responseMessage = BookStoreApp.retrieveAll().toString
-        createConnection(responseMessage)
+
       }else if (function.equalsIgnoreCase("findById")){
         responseMessage = BookStoreApp.findById(array(1).toInt).toString
-        createConnection(responseMessage)
+
       }else if (function.equalsIgnoreCase("removeById")){
         BookStoreApp.removeById(array(1).toInt)
         responseMessage = "REMOVED"
-        createConnection(responseMessage)
+
       }else if (function.equalsIgnoreCase("save")){
         val book = Book(array(1),array(2),array(3).toDouble,array(4))
         BookStoreApp.save(book)
         responseMessage = "Saved " + book + "successfully"
-        createConnection(responseMessage)
+
       }else if (function.equalsIgnoreCase("update")){
         val id = array(1).toInt
         val book = Book(array(2),array(3),array(4).toDouble,array(5))
         BookStoreApp.updateBookData(id,book)
         responseMessage = "Updated " + book
-        createConnection(responseMessage)
+
+      }else {
+        responseMessage = "INVALID REQUEST"
       }
-      //createConnection(responseMessage)
+      createReplyConnection(responseMessage)
     }
     channel.basicConsume(queueName, true, deliverCallback, _ => {})
   }
 
 
-  def createConnection (message : String) : Unit = {
-    val factory = new ConnectionFactory()
-    factory.setHost("localhost")
+  def createReplyConnection (message : String) : Unit = {
     val connection = factory.newConnection()
-    val channel = connection.createChannel()
-    channel.exchangeDeclare(EXCHANGE_NAME, "direct")
-    channel.basicPublish(EXCHANGE_NAME, BINDING_KEY_RESPONSE, null, message.getBytes("UTF-8"))
+    val channel2 = connection.createChannel()
+    channel2.exchangeDeclare(EXCHANGE_NAME_RES, "direct")
+    channel2.basicPublish(EXCHANGE_NAME_RES, BINDING_KEY_RESPONSE, null, message.getBytes("UTF-8"))
     println(" [x] Sent '" + message + "'")
-    channel.close()
-    connection.close()
   }
 
 
